5f6b21e95ab254f12c5a8710b5ed7823312ad830
marriola <azathoth500@gmail.com>
2015-06-06 16:53:19 -0400

    Added test for multiple roles intervals.

--- RoleParserTest/UnitTest1.cs
+++ RoleParserTest/UnitTest1.cs
@@ -9,13 +9,18 @@ namespace RoleParserTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestYears()
+        public void TestRoleParser()
         {
             BandMember member = new BandMember("John Doe", "Guitar, vocals (1992-2015)");
             Assert.AreEqual("John Doe: Guitar, vocals (1992-2015)", member.ToString());
 
-            member = new BandMember("Jane Doe", "Bass (1992-1995, 2003-present)");
-            Assert.AreEqual("Jane Doe: Bass (1992-1995, 2003-2015)", member.ToString());
+            // A year range ending in "present" is just replaced with the
+            // current year, since for the purposes of generating a lineup
+            // chart it doesn't really matter if the member stopped that year
+            // or not. BandMember.ToString's only purpose is for testing that
+            // the information was parsed correctly.
+            member = new BandMember("Jane Doe", "Bass (1992-1995, 2003-present), drums (1995-2003)");
+            Assert.AreEqual("Jane Doe: Bass (1992-1995, 2003-2015), drums (1995-2003)", member.ToString());
         }
     }
 }
