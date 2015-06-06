3d5747445fc866bb28fe34fc7cefd60dab14c122
marriola <azathoth500@gmail.com>
2015-06-06 10:56:50 -0400

    Added unit test for BandMember role parser.

---
+++ RoleParserTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using LineupScraper;
+
+namespace RoleParserTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestYears()
+        {
+            BandMember member = new BandMember("John Doe", "Guitar, vocals (1992-2015)");
+            Assert.AreEqual("John Doe: Guitar, vocals (1992-2015)", member.ToString());
+        }
+    }
+}
